21f830154f1e2596990d766f3664b3b276b4e0d4
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-11-05 14:38:32 -0300

    Update InitState.hx

--- source/InitState.hx
+++ source/InitState.hx
@@ -5,7 +5,7 @@ import flixel.FlxG;
 
 class InitState extends Sprite
 {        
-        var BG:FlxSprite = new FlxSprite().loadGraphic(Paths.image('telasaleatoras/telaaleatoria-' + FlxG.random.int(1, 3)));
+        var BG:FlxSprite = new FlxSprite().loadGraphic(Paths.image('telasaleatorias/telaaleatoria-' + FlxG.random.int(1, 3)));
 	BG.screenCenter();
 	add(BG);
 }
