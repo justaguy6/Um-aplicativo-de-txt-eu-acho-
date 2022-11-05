e7a4af335ca6236c62144ee28e032b3bfad69d2b
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-11-05 14:03:29 -0300

    xd

--- source/InitState.hx
+++ source/InitState.hx
@@ -7,8 +7,8 @@ import flixel.FlxSprite:
     
         var fundo:FlxSprite;
         
-				fundo = new FlxSprite(-650, -100).makeGraphic(FlxG.width, FlxG.height, FlxColor.WHITE);
-				fundo.scale.set(5, 5);
-				fundo.updateHitbox();
-				add(fundo);
+	fundo = new FlxSprite(-650, -100).makeGraphic(FlxG.width, FlxG.height, FlxColor.WHITE);
+	fundo.scale.set(5, 5);
+	fundo.updateHitbox();
+	add(fundo);
   }
