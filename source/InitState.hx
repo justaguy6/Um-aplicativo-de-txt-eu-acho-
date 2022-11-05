b8eb0733fa65662141cd5ef3c5ac017de1c545d1
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-11-05 14:21:32 -0300

    Update InitState.hx

--- source/InitState.hx
+++ source/InitState.hx
@@ -8,7 +8,7 @@ class InitState extends Sprite
     
         var fundo:FlxSprite;
         
-        fundo:FlxSprite = new FlxSprite().loadGraphic(Paths.image('telas/aleatorias-' + FlxG.random.int(1, 4)));
+        fundo:FlxSprite = new FlxSprite().loadGraphic(Paths.image('telas/aleatoria-' + FlxG.random.int(1, 4)));
 	fundo.scale.set(5, 5);
 	fundo.updateHitbox();
 	add(fundo);
