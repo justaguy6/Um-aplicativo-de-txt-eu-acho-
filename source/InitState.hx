493ac7ce153f77b8349bb056f439282af1609395
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-11-05 14:02:59 -0300

    Create InitState.hx

---
+++ source/InitState.hx
@@ -0,0 +1,14 @@
+package;
+
+import flixel.FlxSprite:
+  
+  class InitState extends Sprite
+  {
+    
+        var fundo:FlxSprite;
+        
+				fundo = new FlxSprite(-650, -100).makeGraphic(FlxG.width, FlxG.height, FlxColor.WHITE);
+				fundo.scale.set(5, 5);
+				fundo.updateHitbox();
+				add(fundo);
+  }
