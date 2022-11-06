ba1c1dbfc8a59d798071003092f184f218860acb
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-11-06 11:57:29 -0300

    Create FileSystemTypes.hx

---
+++ source/FileSystemTypes.hx
@@ -0,0 +1,27 @@
+package;
+
+#if sys
+import sys.io.File;
+import sys.FileSystem;
+#end
+  
+class FileSystemTypes 
+{
+       (for i in FileSystem.readDirectory(FileSystem.absolutePath("assets/songs")))
+        
+                 FileSystem.exists("assets/images/archive")
+                            FileSystem.absolutePath("assets/songs")
+                              
+                              
+                              
+                 public function FileSystem Directory Types
+                 {
+                   
+                            FileSystem.createDirectory("assets/example")
+                                    
+                                    
+                            (for i in FileSystem.readDirectory(FileSystem.absolutePath("assets/data") // usado como precarregamento de arquivos  
+                 }
+                       // esse codigo ira ser atualizado em breve conforme meus aprendizados (;
+}
+  
